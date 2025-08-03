a36c1eab9a56d73e3c242f3812ed379e8cc54891
Maximo2410 <m4x1mo.vgz@gmail.com>
2025-08-03 15:18:43 -0300

    Update backend.js

--- assets/scripts/backend.js
+++ assets/scripts/backend.js
@@ -1,6 +1,6 @@
 async function cargarScriptProtegido(nombreScript) {
         try {
-            const response = await fetch(`./backend/get_script.php?script=${nombreScript}`, {
+            const response = await fetch(`/backend/get_script.php?script=${nombreScript}`, {
                 headers: {
                     'X-API-Key': 'tu_clave_secreta_123',
                     'Authorization': `Bearer ${localStorage.getItem('authToken')}`
